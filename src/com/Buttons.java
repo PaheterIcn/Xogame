ae730bc833d76d30a9fbdaec97351ebb10b4861e
paheter <36377359+paheter@users.noreply.github.com>
2021-05-21 11:12:11 +0700

    XO
    save

---
+++ src/com/Buttons.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author 5543
+ */
+public class Buttons {
+    boolean buttons1 = true;
+    boolean buttons2 = true;
+    boolean buttons3 = true;
+    boolean buttons4 = true;
+    boolean buttons5 = true;
+    boolean buttons6 = true;
+    boolean buttons7 = true;
+    boolean buttons8 = true;
+    boolean buttons9 = true;
+}
